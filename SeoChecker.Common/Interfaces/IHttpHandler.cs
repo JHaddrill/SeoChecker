f6fd91d3f69ec58748bdc86c8c1db8a36563eb79
JHaddrill <jhaddrill@live.com.au>
2021-04-14 22:04:06 +1000

    Added HttpHandler to handle webpage requests

---
+++ SeoChecker.Common/Interfaces/IHttpHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SeoChecker.Common.Interfaces
+{
+    public interface IHttpHandler
+    {
+        public Task<string> GetWebpageAsStringAsync(string query);
+    }
+}
---
+++ SeoChecker.Common/Services/HttpHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using SeoChecker.Common.Interfaces;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SeoChecker.Common.Services
+{
+    public class HttpHandler : IHttpHandler
+    {
+        private readonly ILogger<HttpHandler> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HttpHandler(ILogger<HttpHandler> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<string> GetWebpageAsStringAsync(string query)
+        {
+            string webpage = string.Empty;
+            using (var client = _clientFactory.CreateClient())
+            {
+                _logger.LogTrace($"Sending GET Request: {query}");
+                var result = await client.GetAsync(query);
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogTrace($"Recieved successful response from {query}");
+                    webpage = await result.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    _logger.LogTrace($"Unable to retrieve webpage from {query}");
+                }
+            }
+            return webpage;
+        }
+    }
+}
